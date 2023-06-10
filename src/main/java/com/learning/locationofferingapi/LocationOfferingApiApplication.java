a1241c2b259e115d469f36c3f343183bdb7c1137
gp15051991 <gp15051991@gmail.com>
2023-06-10 14:54:33 -0600

    Initial Commit

---
+++ src/main/java/com/learning/locationofferingapi/LocationOfferingApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.locationofferingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationOfferingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationOfferingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/locationofferingapi/LocationOfferingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.locationofferingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationOfferingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
